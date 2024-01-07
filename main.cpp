71883a8bf6fae4bcceca5d73feaa5e3e7b3454f8
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2024-01-07 18:18:51 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int speed;
+    std::cout << "Enter aircraft speed: ";
+    std::cin >> speed;
+
+    int height;
+    std::cout << "Enter flight altitude: ";
+    std::cin >> height;
+
+    if(speed < 750 || speed > 850 || height < 9000 || height > 9500)
+        std::cout << "The echelon is not correct!!!";
+    else
+        std::cout << "Correct echelon!!!";
 }
